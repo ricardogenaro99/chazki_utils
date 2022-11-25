7515c6c60be254343296970e09bef8a8b2d67363
Ricardo Genaro <genaro.choquehuanca.palli@gmail.com>
2022-11-25 13:31:13 -0500

    feat: stock now

--- insertar-stock/querys.js
+++ insertar-stock/querys.js
@@ -3,7 +3,7 @@ const query = ({ warehouseID, enterpriseId, trackcode, cityID }) => {
   INSERT INTO public."Stock"
   ("locationBranchID", "orderID", "enterDate", "statusID", quantity, dispatched, "dispatchedDate", 
   "createdAt", "updatedAt", "quantityDispatch", "warehouseID", "enterpriseID", deleted, "trackCode", "cityID", "userIDCreation", "userIDDispatch")
-  VALUES(NULL, NULL, NULL, 209, 1, false, NULL, '2022-11-04 10:33:04.423', '2022-11-04 10:33:04.423', NULL, ${warehouseID}, ${enterpriseId}, false, '${trackcode}', ${cityID}, NULL, NULL);
+  VALUES(NULL, NULL, NULL, 209, 1, false, NULL, now(), now(), NULL, ${warehouseID}, ${enterpriseId}, false, '${trackcode}', ${cityID}, NULL, NULL);
 `;
 	return queryInsert;
 };
